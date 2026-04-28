5d6193dc35ae1a2c415babadff7c4d46d6a2c124
haris49 <hariis.459@gmail.com>
2026-04-28 11:26:52 +0700

    Menambahkan class DoubleLinkedList dengan constructor dan fungsi addNode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -10,3 +10,17 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
