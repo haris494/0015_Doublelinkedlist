32b3444244a18116947cdd2cc6feb31465ceb75c
haris49 <hariis.459@gmail.com>
2026-04-28 12:11:51 +0700

    Menambahkan fungsi revtraverse dan proses menuju node terakhir

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -153,8 +153,24 @@ void traverse()
     }
 }
 
+void revtraverse()
+{
+    if (START == NULL)
+    {
+        cout << "\nList is empty" << endl;
+        return;
+    }
+
+    // Step 1: Move to last node
+    Node *currentNode = START;
+    int i = 0;
+    while (currentNode->next != NULL)
+    {
+        currentNode = currentNode->next;
+        i++;
+    }
 
 
-    
+
 
 
